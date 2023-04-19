7c886f97d35db41036dc84745b27cbf295d2b73c
Jorge Piqueras García-Ligero <jorgepiqueras92@gmail.com>
2023-04-19 14:56:35 +0000

    fix: admin fixed

--- src/api/admin.py
+++ src/api/admin.py
@@ -11,7 +11,8 @@ def setup_admin(app):
 
     
     # Add your models here, for example this is how we add a the User model to the admin
-    admin.add_view(ModelView(Technician, Service, db.session))
+    admin.add_view(ModelView(Service, db.session))
+    admin.add_view(ModelView(Technician, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
