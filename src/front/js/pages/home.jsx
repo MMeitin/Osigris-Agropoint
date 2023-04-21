2e270484425b040a54ed66fe324d0b7d96a66475
Jorge Piqueras García-Ligero <jorgepiqueras92@gmail.com>
2023-04-21 22:11:16 +0000

    Navbar and first part landing page

--- src/front/js/pages/home.jsx
+++ src/front/js/pages/home.jsx
@@ -4,50 +4,49 @@ import { Context } from "../store/appContext";
 import "../../styles/home.css";
 
 export const Home = () => {
-	
-	  return (
-		<div className="App">
-		  <header>
-			<button>Iniciar sesión</button>
-			<h1>Encuentra a tu técnico</h1>
-			<div>
-			  <input type="text" placeholder="Buscar" />
-			  <select>
-				<option>Opción 1</option>
-				
-			  </select>
-			  <select>
-				<option>Opción A</option>
-				<option>Opción B</option>
-				<option>Opción C</option>
-			  </select>
-			</div>
-		  </header>
-		  <div className="favorites">
-			<h2>Destacados</h2>
-			<div className="card-row">
-			  <div className="card">Card 1</div>
-			  <div className="card">Card 2</div>
-			  <div className="card">Card 3</div>
-			  <div className="card">Card 4</div>
-			</div>
-		  </div>
-		  <div className="content">
-			<h2>Contenido</h2>
-			<div className="card-columns">
-			  <div className="card">Card 5</div>
-			  <div className="card">Card 6</div>
-			  <div className="card">Card 7</div>
-			  <div className="card">Card 8</div>
-			  <div className="card">Card 9</div>
-			  <div className="card">Card 10</div>
-			  <div className="card">Card 11</div>
-			  <div className="card">Card 12</div>
-			</div>
-		  </div>
-		</div>
-	  );
-	}
-	
-	export default App;
-	
+  return (
+    <div>
+      
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="col2 ms-5">
+          <h2>LOGO</h2>
+        </div>
+        <div className="d-flex col justify-content-end mb-3 p-4 px-5">
+          <a className="navbar-brand mb-0 h1 p-2 px-5" href="#">
+            Sección 1
+          </a>
+          <a className="navbar-brand mb-0 h1 p-2 px-5" href="#">
+            Sección 2
+          </a>
+          <a className="navbar-brand mb-0 h1 p-2 px-5" href="#">
+            Sección 3
+          </a>
+          <button className="btn">Inicio de sesión</button>
+        </div>
+      </nav>
+
+      
+      <div className="container-fluid  row">
+        <div className="textos row col-6 d-flex align-items-center justify-content-center">
+          <h1 >NOS ADAPTAMOS A TI</h1>
+          <p >Vas a poder encontrar al técnico que más se acomode a tus necesidades em tu ciudad. Crea</p>
+        </div>
+        <div className=" col-6 ">
+          <img className="img1"
+            src="https://res.cloudinary.com/ddyd5ebc7/image/upload/v1682015035/_dde26f94-d3f6-44ef-a892-4c90bd2c8dd6_rorddp.png"
+            alt="Imagen de la sección 1"
+          />
+        </div>
+      </div>
+
+      {/* Sección 2 */}
+      <div className="section">
+        <h1>Título de la sección 2</h1>
+        <p>Párrafo de la sección 2</p>
+        <img src="ruta-a-la-imagen-2.jpg" alt="Imagen de la sección 2" />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
