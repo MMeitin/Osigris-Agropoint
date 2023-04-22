ff9fbc4ef2c1cd3ce760ce585aa5656b403df458
Jorge Piqueras García-Ligero <jorgepiqueras92@gmail.com>
2023-04-22 11:02:30 +0000

    Cards en la landin

--- src/front/js/pages/home.jsx
+++ src/front/js/pages/home.jsx
@@ -25,14 +25,19 @@ export const Home = () => {
         </div>
       </nav>
 
+      <div className="body container-fluid">
+
+      <div className="titulo col-12 d-flex justify-content-center">
+        <h1>Encuentra a tu <span>técnico</span></h1>
+      </div>
       
       <div className="container-fluid  row">
         <div className="textos row col-6 d-flex align-items-center justify-content-center">
-          <h1 >NOS ADAPTAMOS A TI</h1>
+          <h1 >¿Qué necesitas?</h1>
           <p >Vas a poder encontrar al técnico que más se acomode a tus necesidades em tu ciudad. Crea</p>
         </div>
-        <div className=" col-6 ">
-          <img className="img1"
+        <div className=" row col-4 d-flex  ">
+          <img className="img1 img-fluid"
             src="https://res.cloudinary.com/ddyd5ebc7/image/upload/v1682015035/_dde26f94-d3f6-44ef-a892-4c90bd2c8dd6_rorddp.png"
             alt="Imagen de la sección 1"
           />
@@ -40,11 +45,42 @@ export const Home = () => {
       </div>
 
       {/* Sección 2 */}
-      <div className="section">
-        <h1>Título de la sección 2</h1>
-        <p>Párrafo de la sección 2</p>
-        <img src="ruta-a-la-imagen-2.jpg" alt="Imagen de la sección 2" />
-      </div>
+      
+      
+        <div className="section2 col-12 d-flex align-items-center justify-content-center flex-column text-center">
+          <h1 className="row">Título de la sección 2</h1>
+          <p className="row">Párrafo de la sección 2</p>
+          {/*<img src="ruta-a-la-imagen-2.jpg" alt="Imagen de la sección 2" />*/}
+        </div>
+        <div className="container row">
+        <div className="card col-3" >
+          <img  className="img1 img-fluid"src="https://elcamponopara.org/wp-content/uploads/2020/04/oferta-INGENIERO-T%C3%89CNICO-AGR%C3%8DCOLA-E-INDUSTRIAL.jpg" className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        <div className="card col-3" >
+          <img className="img1 img-fluid" src="https://www.marismas.es/wp-content/uploads/2018/06/asesoramiento-tecnico-agricola-4.jpg" className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        <div className="card col-3" >
+          <img src="..." className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        </div>
+      
+      
+    </div>
     </div>
   );
 };
