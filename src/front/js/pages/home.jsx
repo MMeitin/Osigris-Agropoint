185211359caf737248aeb2cf367acf09e14fe4ab
Jorge Piqueras García-Ligero <jorgepiqueras92@gmail.com>
2023-04-23 11:07:09 +0000

    feat: metidos cards y dudas

--- src/front/js/pages/home.jsx
+++ src/front/js/pages/home.jsx
@@ -27,14 +27,16 @@ export const Home = () => {
 
       <div className="body container-fluid">
 
-      <div className="titulo col-12 d-flex justify-content-center">
+      <div className="titulo col-12 d-flex justify-content-center m-5">
         <h1>Encuentra a tu <span>técnico</span></h1>
       </div>
       
       <div className="container-fluid  row">
         <div className="textos row col-6 d-flex align-items-center justify-content-center">
-          <h1 >¿Qué necesitas?</h1>
-          <p >Vas a poder encontrar al técnico que más se acomode a tus necesidades em tu ciudad. Crea</p>
+          <h1 className="titulo1">El <span>técnico agrícola</span> perfecto para ti.</h1>
+          <p >Nuestra empresa es la solución ideal para los agricultores que buscan un técnico agrícola especializado en su cultivo.
+             Con nuestra plataforma, podrás encontrar rápidamente al profesional adecuado, con amplia experiencia y conocimientos para mejorar tus cosechas.
+               No pierdas más tiempo buscando al técnico agrícola ideal, ¡únete a nosotros y haz crecer tu negocio hoy mismo!</p>
         </div>
         <div className=" row col-4 d-flex  ">
           <img className="img1 img-fluid"
@@ -47,30 +49,41 @@ export const Home = () => {
       {/* Sección 2 */}
       
       
-        <div className="section2 col-12 d-flex align-items-center justify-content-center flex-column text-center">
-          <h1 className="row">Título de la sección 2</h1>
-          <p className="row">Párrafo de la sección 2</p>
+        <div className="section2 col-8 d-flex align-items-center justify-content-center flex-column text-center mx-auto m-5">
+          <h1 className="titulo2">¿Buscando al gurú verde para tus <span>cultivos</span>? <br></br>¡Aquí lo tienes!</h1>
+          <p className="row">  Nuestros técnicos agrícolas especializados son la clave para mejorar la producción de tus cultivos. 
+          Con años de experiencia en el campo y conocimientos actualizados, te brindarán una asesoría de calidad, personalizada y adaptada a tus necesidades. 
+          Encontrar al técnico agrícola perfecto nunca ha sido tan fácil como con nuestra plataforma. ¡conoce a nuestros técnicos agrícolas especializados hoy mismo!</p>
           {/*<img src="ruta-a-la-imagen-2.jpg" alt="Imagen de la sección 2" />*/}
         </div>
-        <div className="container row">
-        <div className="card col-3" >
-          <img  className="img1 img-fluid"src="https://elcamponopara.org/wp-content/uploads/2020/04/oferta-INGENIERO-T%C3%89CNICO-AGR%C3%8DCOLA-E-INDUSTRIAL.jpg" className="card-img-top" alt="..."/>
+        <div className=" row  justify-content-center">
+        
+        <div className=" card col-3  m-3" >
+          <img  src="https://elcamponopara.org/wp-content/uploads/2020/04/oferta-INGENIERO-T%C3%89CNICO-AGR%C3%8DCOLA-E-INDUSTRIAL.jpg"  className="card-img-top  "alt="..."/>
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             <a href="#" className="btn btn-primary">Go somewhere</a>
           </div>
         </div>
-        <div className="card col-3" >
-          <img className="img1 img-fluid" src="https://www.marismas.es/wp-content/uploads/2018/06/asesoramiento-tecnico-agricola-4.jpg" className="card-img-top" alt="..."/>
+        <div className=" card col-3  m-3 " >
+          <img className="card-img-top " src="https://www.marismas.es/wp-content/uploads/2018/06/asesoramiento-tecnico-agricola-4.jpg" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             <a href="#" className="btn btn-primary">Go somewhere</a>
           </div>
         </div>
-        <div className="card col-3" >
-          <img src="..." className="card-img-top" alt="..."/>
+        <div className=" card col-3 m-3" >
+          <img src="https://www.empresaagraria.com/wp-content/uploads/2020/05/Gonzalo-P%C3%A9rez-Fern%C3%A1ndez-1179x580.jpeg" className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        <div className=" card col-3 m-3" >
+          <img src="https://www.empresaagraria.com/wp-content/uploads/2020/05/Gonzalo-P%C3%A9rez-Fern%C3%A1ndez-1179x580.jpeg" className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -78,6 +91,38 @@ export const Home = () => {
           </div>
         </div>
         </div>
+        <div className="section3 col-12 d-flex align-items-center justify-content-center flex-column text-center">
+        
+          <h1 className="row">¿Dudas?</h1>
+
+          <div className="container col-8 d-flex align-items-center justify-content-center flex-column text-center">
+            <div className="col-lg-9 col-md-12 col-sm-12 d-flex flex-column justify-content-center align-items-center">
+              <div className="d-flex align-items-center">
+                <i className="fas fa-question-circle fa-3x mr-2"></i>
+                <h4>¿Cómo puedo registrarme en la plataforma?</h4>
+              </div>
+              <p>Para registrarte en nuestra plataforma, simplemente haz clic en el botón 'Iniciar sesión' luego pulsa en registrarte y completa el formulario con tus datos personales y de contacto.</p>
+            </div>
+            <div className="col-lg-9 col-md-12 col-sm-12 d-flex flex-column justify-content-center align-items-center">
+              <div className="d-flex align-items-center">
+                <i className="fas fa-question-circle fa-3x mr-2"></i>
+                <h4>¿Cómo puedo contactar a un técnico agrícola?</h4>
+              </div>
+              <p>Después de registrarte en la plataforma, podrás buscar técnicos agrícolas especializados y contactarlos directamente a través de nuestro sistema de mensajería interno.</p>
+            </div>
+            
+            <div className="col-lg-9 col-md-12 col-sm-12 d-flex flex-column justify-content-center align-items-center">
+              <div className="d-flex align-items-center">
+                <i className="fas fa-question-circle fa-3x mr-2"></i>
+                <h4>¿Cómo sé cuál técnico agrícola es el más adecuado para mi proyecto?</h4>
+              </div>
+              <p>Cada técnico agrícola en nuestra plataforma tiene un perfil detallado que describe su experiencia, habilidades y áreas de especialización. Al buscar un técnico agrícola, puedes filtrar por ubicación, especialización y otros criterios para encontrar al más adecuado para tu proyecto. Además, puedes ver las calificaciones y comentarios dejados por otros clientes anteriores para ayudarte a tomar una decisión informada.</p>
+            </div>  
+
+            </div>
+          </div>
+
+
       
       
     </div>
